009f8567bebfca9d84ee41b523bcbc92fc8b719a
root <mayhoral0008@gmail.com>
2022-03-11 10:56:52 -0800

    ds

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+/**
+ * main - Entry poing
+ *
+ * Description: print "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+ {		
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+ }

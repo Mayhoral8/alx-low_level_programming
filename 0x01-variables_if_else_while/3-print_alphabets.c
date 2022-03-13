7a787a4ca500eebc340eda3c711bcc8b7059051e
root <mayhoral0008@gmail.com>
2022-03-13 11:31:21 -0700

    f

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - Entry point
  *
  * Description - let's get running
@@ -10,6 +10,7 @@ int main(void)
 {
 	int i = 97;
 	int j = 65;
+
 	while (i < 123)
 	{
 		putchar(i);

908b1f52c9c066ce2216ddddedd0f9db6a2b45a1
root <mayhoral0008@gmail.com>
2022-03-13 15:28:58 -0700

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints alphabets except e and q
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i != 101 && i != 113 )
+		putchar(i);
+		i++;
+		return (0);
+
+}

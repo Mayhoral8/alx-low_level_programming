e79c552df23ce7af9cbb86a77d848878ed3ecfee
root <mayhoral0008@gmail.com>
2022-03-13 11:22:19 -0700

    f

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/** main - Entry point
+ *
+ * Description - let's get running
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	while (i < 91 )
+	{
+		putchar(i);
+		i++;
+	{
+	putchar(10);
+	return (0);
+
+}

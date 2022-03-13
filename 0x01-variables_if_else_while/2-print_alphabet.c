79b7675d57d12ee50782e630738c935d1498bb92
root <mayhoral0008@gmail.com>
2022-03-13 11:07:24 -0700

    f

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,12 +10,15 @@
 
 int main(void)
 {
-	int norm;
-	int lwcase;
+	int i = 97;
 
-	norm = putchar(65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,
-			86,87,88,89,90);
-	lwcase = tolower(norm);
+	while (i< 127)
+	{
+		putchar(i);
+			i++;
+	}
+	putchar(10);
+
 	return (0);
 
 }

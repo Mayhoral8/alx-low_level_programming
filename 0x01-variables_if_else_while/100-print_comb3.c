595a390a2338dfb5f7eda2f674cf797095769d97
root <mayhoral0008@gmail.com>
2022-03-14 04:19:43 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: combination of two 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+
+	while (i < 58)
+	{
+	putchar(i);
+		if (i != j and j <= 57 )
+		{
+			putchar(j);
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}	
+	putchar(10);
+	return (0);
+}
+

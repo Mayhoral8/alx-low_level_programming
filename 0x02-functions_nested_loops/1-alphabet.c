75b0e13d300d96c01c65ba7885035719e92b67e7
root <mayhoral0008@gmail.com>
2022-03-15 11:08:52 -0700

    e

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Alphabet again
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letters <= z)
+		_putchar(letters);
+	_putchar('\n');
+		return (0);
+}
+	

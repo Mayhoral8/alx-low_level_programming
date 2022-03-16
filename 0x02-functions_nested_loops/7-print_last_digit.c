76b3a78d1e77d9183e0d2aa58b70a79736f43b87
root <mayhoral0008@gmail.com>
2022-03-16 07:44:49 -0700

    fd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,12 @@ int print_last_digit(int n)
 
 	int lastDigit = n % 10;
 	if (n < 0)
-	{
-		lastDigit = lastDigit * -1;
-			
-		_putchar(lastDigit + '0');
 
-		return (lastDigit);
-	}
-	
+	lastDigit = lastDigit * -1;
+			
+	_putchar(lastDigit + '0');
+
+	return (lastDigit);
 }
+	
+

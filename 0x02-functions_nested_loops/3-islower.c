83a825ae549f204c0ffa2a773f0379aa062131d2
root <mayhoral0008@gmail.com>
2022-03-16 02:53:49 -0700

    e

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * _islower - program
+ *
+ * Description: program that checks lower case
+ *
+ * Return: 1 if lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c<= 'z')
+	{
+		return (1);
+	}
+	else
+	}	
+	return (0);
+	}
+
+}

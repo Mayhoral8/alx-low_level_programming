9968f0b059044afc09b087e8df63ff9e01e51b0a
root <mayhoral0008@gmail.com>
2022-03-16 05:08:32 -0700

    all

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ *
+ * Description: Program that check for alphabets
+ *
+ * @c: c is an ascii character
+ *
+ * Return: 1 if it's not a letter, lowercase or uppercase
+ */
+int _isalpha(int c)
+{
+	if (c >=a 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+		
+}

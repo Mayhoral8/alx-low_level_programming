be337b57984b8ee2969ff4ed1ef811cc298c023e
root <mayhoral0008@gmail.com>
2022-03-16 12:22:50 -0700

    f

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Program that adds two integers
+ *
+ * Description: ds
+ *
+ * Return: added
+ */
+int add(int x, int y)
+{
+	add = x + y;
+	result = add(x, y);
+	return (result);
+}

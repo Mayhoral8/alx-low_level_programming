8075560a2eff7d5ade8622f51d0addeb6ab86b6b
root <mayhoral0008@gmail.com>
2022-03-16 05:34:05 -0700

    o

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign — Entrt point
+ *
+ * Description: program that prints sign
+ *
+ * @c: an ascii character
+ *
+ * Return: 1 if +, 0 if 0 -1, if -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+')
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0')
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

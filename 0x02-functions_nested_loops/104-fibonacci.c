20495e66cf6727f64532aa9a10b03fa97206d09f
root <mayhoral0008@gmail.com>
2022-03-17 02:52:12 -0700

    f

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints the fibonacci numbers
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+	int sum, first, second;
+	sum = 0;
+	first = 0;
+	second = 1;
+
+	while (sum < 98)
+	{
+		sum = first + second;
+		
+		first = second;
+
+		second = sum;
+
+	
+	
+	}
+	printf("%i, %c, %c\n", sum, ",", " ");
+
+	return (0);
+
+}

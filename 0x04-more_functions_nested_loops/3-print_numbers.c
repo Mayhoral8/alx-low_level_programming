c2b069f6aa9011c88ef66f3ed9426f575b3ad77c
root <mayhoral0008@gmail.com>
2022-03-18 11:43:24 -0700

    f

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - program 
+ *
+ * Description: print
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	a = 48;
+
+	while (a < 58)
+	{
+		_putchar(a);
+		i++;
+	} 
+	return (0);
+}

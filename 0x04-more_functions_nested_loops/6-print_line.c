a635285789c816d634042e58cf57fcce92050837
root <mayhoral0008@gmail.com>
2022-03-19 01:50:11 -0700

    f

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * print_line - program that prints a straight line
+ * @n: integer
+ * Return: void
+ */
+void print_line(int n)
+{
+	int line = 0;
+
+	while (line < n)
+	{
+		_putchar('_')
+		line++;
+	}
+	return (0);
+}

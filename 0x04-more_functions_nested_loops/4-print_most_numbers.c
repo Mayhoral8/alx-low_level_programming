4d5591d8af4288faafd4111500299112a420d2d6
root <mayhoral0008@gmail.com>
2022-03-18 21:40:25 -0700

    f

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - gf
+ * Description: g
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int a;
+i
+	for (a = '0'; a < '9'; a++)
+	{
+
+		if (a != 2 && a != 4 )
+		{
+			_putchar(a);
+		
+		}
+	
+	}
+	_putchar("\n");
+	return (0);
+}

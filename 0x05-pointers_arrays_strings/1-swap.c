43ce1b76f33e0c04f2327361ab4a6cfd4df900dd
root <mayhoral0008@gmail.com>
2022-03-22 15:44:11 -0700

    g

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.c"
+/**
+ * swap_int - program that swaps
+ *
+ * @a: variable
+ * @b: variable
+ * Return: Always
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+	int c;
+	*a = &n;
+	*b = *c;
+}

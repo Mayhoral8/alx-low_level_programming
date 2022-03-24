f3aab959b3c831025a9427f0fdfd8e5a637a99b9
root <mayhoral0008@gmail.com>
2022-03-24 04:56:55 -0700

    f

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @*str: A string character
+ */
+void _puts(char *str)
+{
+	char *str;
+	char a;
+	char string[] = "";
+	a = string
+	str = &a;
+}

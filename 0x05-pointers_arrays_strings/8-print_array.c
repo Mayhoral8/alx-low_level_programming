925fb7f5cbeb1d9b80570213fbb41c004fb07af2
root <mayhoral0008@gmail.com>
2022-03-29 07:14:23 -0700

    g

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
         i = 0;
         for (n--; n >= 0; n--, i++)
         {
-                printf("%d", a[i]);
+                _putchar("%d", a[i]);
                 if (n > 0)
                 {
-                        printf(", ");
+                        _putchar(", ");
                 }
         }
-        printf("\n");
+        _putchar("\n");
 
 }
